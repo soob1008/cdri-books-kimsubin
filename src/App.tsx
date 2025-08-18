00302d9096edfcd38643146bbb4c99e9cc8a8284
subin kim <1008sb354@gmail.com>
2025-08-18 22:24:36 +0900

    chore: react-query 설치 및 적용, eslint-plugin, devtools 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import './App.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
