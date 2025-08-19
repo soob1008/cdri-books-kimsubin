4d3036710ae10a73010185647a950c5782faaa99
subin kim <1008sb354@gmail.com>
2025-08-19 16:28:13 +0900

    feat: Select 컴포넌트 추가, @radix-ui/react-select 설치

---
+++ src/shared/ui/select.tsx
@@ -0,0 +1,44 @@
+import * as Select from '@radix-ui/react-select';
+import DropIcon from '@assets/icons/icon_arrow.svg?react';
+import { twMerge } from 'tailwind-merge';
+
+interface SelectBoxProps {
+  options: { value: string; label: string }[];
+  className?: string;
+}
+
+export default function SelectBox({ options = [], className }: SelectBoxProps) {
+  return (
+    <Select.Root>
+      <Select.Trigger
+        className={twMerge(
+          `group flex items-center justify-between w-full px-2 py-3 border-b border-[#D2D6DA] bg-white text-text-primary t-body-2 focus:outline-none data-[placeholder]:text-text-subtitle`,
+          className
+        )}
+      >
+        <Select.Value placeholder="선택" />
+        <Select.Icon asChild className="">
+          <DropIcon className="group-data-[state=open]:rotate-180" />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content
+          className="mt-[6px] bg-white shadow-[0_0_4px_0_rgba(0,0,0,0.25)] w-[var(--radix-select-trigger-width)] max-h-64 overflow-auto"
+          position="popper"
+        >
+          <Select.Viewport>
+            {options.map((opt) => (
+              <Select.Item
+                key={opt.value}
+                value={opt.value}
+                className="text-sm font-medium px-3 py-1 h-[30px] cursor-pointer text-text-subtitle hover:bg-lightgray/60 data-[state=checked]:bg-lightgray data-[highlighted]:outline-none"
+              >
+                <Select.ItemText>{opt.label}</Select.ItemText>
+              </Select.Item>
+            ))}
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+}
