49695db5f6b86591b8a4177c9e42faf5e226eeed
subin kim <1008sb354@gmail.com>
2025-08-20 21:50:02 +0900

    chore: react queryClient provider 설정 분리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,12 @@
-import { QueryClientProvider } from '@tanstack/react-query';
-import { QueryClient } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@app/providers/router';
-
-const queryClient = new QueryClient();
+import QueryProvider from '@app/providers/queryClient';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+    <QueryProvider>
       <RouterProvider router={router} />
-    </QueryClientProvider>
+    </QueryProvider>
   );
 }
 
